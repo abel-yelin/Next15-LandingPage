00b00e6ad710659de6dfd10f37de80ebb1c8c03d
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-18 10:51:48 +0300

    Pricing section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Counter from "@/components/Counter";
 import Details from "@/components/Details";
+import Download from "@/components/Download";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Lyric from "@/components/Lyric";
+import Pricing from "@/components/Pricing";
 import React from "react";
 
 const Home = () => {
@@ -13,6 +15,8 @@ const Home = () => {
       <Features />
       <Details />
       <Counter />
+      <Pricing />
+      <Download />
     </>
   );
 };
